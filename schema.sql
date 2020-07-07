110ed380dd9605235cc46c1eddfd06b03122cf88
AMA <alexanderabram6440@gmail.com>
2020-07-07 10:14:28 -0500

    created missions and quests tables and created sees.sql file

--- schema.sql
+++ schema.sql
@@ -1,5 +1,26 @@
-DROP IF EXISTS missions;
+DROP DATABASE IF EXISTS missions;
 CREATE DATABASE missions;
 
 USE missions;
 
+CREATE TABLE missions (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    owners VARCHAR(50) NOT NULL,
+    status BOOLEAN NOT NULL,
+    createdAt DATETIME NOT NULL,
+    due VARCHAR(15) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE quests (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    owners VARCHAR(50) NOT NULL,
+    status BOOLEAN NOT NULL,
+    createdAt DATETIME NOT NULL,
+    due VARCHAR(15) NOT NULL,
+    PRIMARY KEY (id)
+);
