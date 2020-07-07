1f0f2435230dd94e7e375033f8edf7d48e951826
Lex <lexcpryor@gmail.com>
2020-07-07 17:56:14 -0500

    quests appear on screen by number

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,13 @@
 USE missions;
 
 INSERT INTO missions (name, description, owners, status, due)
-VALUES ('Our First Mission', 'A Test Mission', 'Alex Abram', false, 1/10/2021);
+VALUES ('Our First Mission', 'A Test Mission', 'Alex ', false, 1/10/2021);
+INSERT INTO missions (name, description, owners, status, due)
+VALUES ('Our 2 Mission', 'A Mission22', 'Alex ', false, 1/10/2021);
 
-INSERT INTO quests (name, description, owners, status, due)
-VALUES ('First Quest', 'Questing', 'Lex Pryor', false, 8/19/2020);
+INSERT INTO quests (name, misId)
+VALUES ('First Quest', 1);
+INSERT INTO quests (name, misId)
+VALUES ('2 Quest', 1);
+INSERT INTO quests (name, misId)
+VALUES ('3 Quest', 2);
