0abf90fc2484ee5cbde7e1b2b45e578fcff0417d
AMA <alexanderabram6440@gmail.com>
2020-07-07 11:14:10 -0500

    changed DATETIME to just DATE

--- schema.sql
+++ schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE missions (
     description VARCHAR(300) NOT NULL,
     owners VARCHAR(50) NOT NULL,
     status BOOLEAN NOT NULL,
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    due VARCHAR(25) NOT NULL,
+    createdAt DATE DEFAULT CURRENT_TIMESTAMP,
+    due DATE NULL,
     PRIMARY KEY (id)
 );
 
@@ -20,7 +20,7 @@ CREATE TABLE quests (
     description VARCHAR(300) NOT NULL,
     owners VARCHAR(50) NOT NULL,
     status BOOLEAN NOT NULL,
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-    due VARCHAR(25) NOT NULL,
+    createdAt DATE DEFAULT CURRENT_TIMESTAMP,
+    due date NULL,
     PRIMARY KEY (id)
 );
