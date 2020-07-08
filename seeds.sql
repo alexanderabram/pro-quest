af7189d795fc22e03f5f5554a688b62729b2cbfa
Lex <lexcpryor@gmail.com>
2020-07-07 23:28:56 -0500

    updated with user db with scores

--- seeds.sql
+++ seeds.sql
@@ -1,9 +1,9 @@
 USE missions;
 
 INSERT INTO missions (name, description, owners, status, due)
-VALUES ('Our First Mission', 'A Test Mission', 'Alex ', false, 1/10/2021);
+VALUES ('Our First Mission', 'A Test Mission', 'Alex', false, 1/10/2021);
 INSERT INTO missions (name, description, owners, status, due)
-VALUES ('Our 2 Mission', 'A Mission22', 'Alex ', false, 1/10/2021);
+VALUES ('Our 2 Mission', 'A Mission22', 'Alex', false, 1/10/2021);
 
 INSERT INTO quests (name, misId)
 VALUES ('First Quest', 1);
@@ -11,3 +11,10 @@ INSERT INTO quests (name, misId)
 VALUES ('2 Quest', 1);
 INSERT INTO quests (name, misId)
 VALUES ('3 Quest', 2);
+
+INSERT INTO users (username)
+VALUES ('Lex');
+INSERT INTO users (username)
+VALUES ('Alex');
+INSERT INTO users (username)
+VALUES ('Grigore');
